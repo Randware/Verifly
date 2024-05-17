f457f5d2d32fc86f1b361a857d524b80812118b9
GHaxZ <ghax.z.yt@proton.me>
2024-05-17 10:54:59 +0200

    Finished showcase frontend

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -1,11 +1,22 @@
 const emailInput = document.getElementById("email-input");
 const emailSubmitButton = document.getElementById("email-submit");
-const validEmailDisplay = document.getElementById("valid-email")
-const invalidEmailDisplay = document.getElementById("invalid-email")
+const validEmailDisplay = document.getElementById("valid-email");
+const invalidEmailDisplay = document.getElementById("invalid-email");
+const emailSubmit = document.getElementById("email-submit");
 
-const emailRegex = new RegExp("[A-Za-z0-9\._%+\\-]+@[A-Za-z0-9\.\\-]+\\.[A-Za-z]{2,}")
+const verificationModal = new bootstrap.Modal(document.getElementById("verification-modal"));
+const verificationEmailPlaceholder = document.getElementById("verification-email-placeholder");
 
-let emailInputSelected = false;
+const verificationCodeInput = document.getElementById("verification-code-input");
+const invalidVerificationCodeDisplay = document.getElementById("invalid-verification-code");
+const verificationCodeSubmit = document.getElementById("verification-code-submit");
+
+const successModal = new bootstrap.Modal(document.getElementById("success-modal"));
+
+const emailRegex = new RegExp("[A-Za-z0-9\._%+\\-]+@[A-Za-z0-9\.\\-]+\\.[A-Za-z]{2,}");
+const emailSentText = "We have sent a verification code to \"$EMAIL$\".";
+
+let verificationCode = "test";
 
 function checkEmail() {
     let input = emailInput.value;
@@ -29,16 +40,68 @@ function invalidEmail() {
     validEmailDisplay.classList.add("invisible")
 }
 
-window.addEventListener('keyup', function () {
-    if(emailInputSelected) {
-        checkEmail();
+function sendVerification() {
+    let email = emailInput.value.trim();
+
+    verificationEmailPlaceholder.innerHTML = emailSentText.replace("$EMAIL$", email);
+
+    verificationModal.show();
+
+    return;
+
+    const requestBody = {
+        email: email,
+        verification_code_length: 10,
+        html: "<p>Here is your verification code: ${verification_code}</p>"
     }
+
+    fetch("http://localhost:3030", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    }).then(res => res.json())
+        .then(data => {
+            verificationCode = data.verification_code;
+        })
+        .catch(err => {
+            displayError(err);
+            console.log(err);
+        });
+}
+
+function checkVerificationCode() {
+    let input = verificationCodeInput.value.trim();
+
+    if(input === verificationCode) {
+        validVerficationCode();
+    } else {
+        invalidVerificationCode();
+    }
+}
+
+function validVerficationCode() {
+    verificationModal.hide();
+    successModal.show();
+}
+
+function invalidVerificationCode() {
+    invalidVerificationCodeDisplay.classList.remove("invisible")
+}
+
+function displayError(error) {
+    window.alert("An error occurred: " + error.message);
+}
+
+verificationCodeSubmit.addEventListener("click", function () {
+    checkVerificationCode();
 });
 
-emailInput.addEventListener('focus', function () {
-    emailInputSelected = true;
+emailSubmit.addEventListener("click", function() {
+    sendVerification();
 });
 
-emailInput.addEventListener('blur', function () {
-    emailInputSelected = false;
+emailInput.addEventListener('input', function () {
+    checkEmail();
 });
